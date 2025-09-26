2bd7f4beee7de21ba565a1f4e2101af7ccb19f74
Prajjwal Pandey <prajjwalpandey641@gmail.com>
2025-09-26 13:58:41 +0530

    restructuring the files

--- banking-dispute-frontend/vite.config.ts
+++ banking-dispute-frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['frontend', 'localhost'],
+    host: '0.0.0.0',
+    port: 4173,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
